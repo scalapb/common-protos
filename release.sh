75b6ddc179341e72a173a7c301583bb82ae47f1e
Nadav Samet <thesamet@gmail.com>
2021-01-30 17:44:43 -0800

    Switch from travis to github workflows

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ -z "${TRAVIS_TAG}" ]; then 
-    echo "Travis tag is unset - will do a snapshot";
-    sbt ci-release
-else 
-    echo "Travis tag is set to ${TRAVIS_TAG}. Making a release";
-    PUBLISH_ONLY=${TRAVIS_TAG%%/*} RELEASE=1 sbt ci-release;
-fi

06d787aad511f43b17c800f0e1fa811338101251
Nadav Samet <thesamet@gmail.com>
2022-01-16 09:23:03 -0800

    Bump up scala versions

--- project/BuildHelpers.scala
+++ project/BuildHelpers.scala
@@ -125,9 +125,9 @@ final case class ProtosProject(
 
 object ProtosProject {
   val Scala211 = "2.11.12"
-  val Scala212 = "2.12.14"
-  val Scala213 = "2.13.6"
-  val Scala3 = "3.0.1"
+  val Scala212 = "2.12.15"
+  val Scala213 = "2.13.8"
+  val Scala3 = "3.1.0"
 
   val versionTag = settingKey[String]("Version tag to use in git")
 
